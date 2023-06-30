0b13fb19f812019a6fe5e3aa01e1f6542aa340f7
Brash92 <82854707+Brash92@users.noreply.github.com>
2023-06-30 11:59:42 +1000

    Update Meraki Update Switchports.ps1

--- Meraki Update Switchports.ps1
+++ Meraki Update Switchports.ps1
@@ -1,33 +1,12 @@
-﻿# This script is designed to take a CSV file complete with switch port numbers, types, names and vlans, and configure it using the Meraki API
-# This script was developed by Brent Morvan
+# This script is designed to take a CSV file complete with switch port numbers, types, names and vlans, and configure it using the Meraki API
+# This script was developed by Brent
 # Double check all variables before running and USE AT YOUR OWN RISK!
 
-#==============================================
-# Read in and validate the Switch serial number
-#==============================================
-$Serial = Read-Host -Prompt 'Please enter the switch serial number (xxxx-xxxx-xxxx)'
-while($serial -notmatch "^([A-Za-z0-9]{4})-([A-Za-z0-9]{4})-([A-Za-z0-9]{4})$")
-{
-    Write-host "Incorrectly formatted serial number. Please try again`n"
-    $Serial = Read-Host -Prompt 'Please enter the switch serial number (xxxx-xxxx-xxxx)'
-}
-#================================================
-
-#==================================
-# Read in and validate the CSV file
-#==================================
-do {
-$CsvFailed = $false
-$CsvLocation = Read-Host -Prompt 'Please enter the full path of the CSV file (Eg. c:\Temp\ports.csv)'
-Try{
-    $ports = Import-Csv -Path "$CsvLocation"
-} catch {
-Write-Host "Unable to find or interpret .csv file`n"
-$CsvFailed = $true}
-}
-while ($CsvFailed)
-
-#====================================
+Write-Host "=============================DISCLAIMER==================================================="
+Write-Host "This script is designed to automate switchport configuration based on a provided CSV file."
+Write-Host "This is not an official script and comes with no guarantees, warranties or support."
+Write-Host "Use At Your Own Risk!"
+Write-Host "==========================================================================================`n"
 
 #====================================
 # Read in and validate the API Key
@@ -56,8 +35,82 @@ while ($APIFailed)
 #========================================================
 
 
+#============================================================
+# Read in and validate the Switch serial number and validate
+#============================================================
+
+do {
+
+$SwitchName = ""
+$SwitchVerification = ""
+
+# Prompt for switch SN# and do basic input validation
+$Serial = Read-Host -Prompt 'Please enter the switch serial number (xxxx-xxxx-xxxx)'
+while($serial -notmatch "^([A-Za-z0-9]{4})-([A-Za-z0-9]{4})-([A-Za-z0-9]{4})$")
+{
+    Write-host "Incorrectly formatted serial number. Please try again`n"
+    $Serial = Read-Host -Prompt 'Please enter the switch serial number (xxxx-xxxx-xxxx)'
+}
+
+# Perform a lookup of the SN# to validate it is in the Organisation, and to get the device name
+Try{
+    # Set the headers with the API Key
+    $headers = @{
+        "Content-Type" = "application/json"
+        "X-Cisco-Meraki-API-Key" = $APIKey
+    }
+        $device = Invoke-RestMethod -Method GET -Uri "https://api.meraki.com/api/v1/devices/$Serial" -Headers $Headers
+            if ($Serial -eq $device.serial)
+                {
+                    $SwitchName = $device.name
+                } 
+    }
+ 
+catch {
+    if( $_.Exception.Response.StatusCode.Value__ -eq 401 )
+        {Write-Host "Incorrect API Key. Please try again`n"}
+    if( $_.Exception.Response.StatusCode.Value__ -eq 403 )
+        {Write-Host "You do not have permissions in this organization.`n"}
+    else{Write-Host "Switch not found in your Organisation. Please try again.`n"}
+        $SwitchVerification = "No"
+      }
+
+# Prompt the user to confirm that this is indeed the switch that they are wanting to make changes on
+#if ($SwitchVerification -notmatch "^(?:Yes\b|No\b)")
+if ($SwitchVerification -notmatch "^(?:Yes\b|No\b|yes\b|no\b|Y\b|N\b|y\b|n\b)")
+{
+$SwitchVerification = Read-Host -Prompt "You are about to edit the switch $SwitchName. Type `"Yes`" to proceed or `"No`" to re-enter the Serial Number"
+}
+while($SwitchVerification -notmatch "^(?:Yes\b|No\b|yes\b|no\b|Y\b|N\b|y\b|n\b)")
+{
+    Write-host "Invalid entry. Please try again.`n"
+    $SwitchVerification = Read-Host -Prompt "You are about to edit the switch $SwitchName. Type `"Yes`" to proceed or `"No`" to re-enter the Serial Number"
+}
+}
+while (($SwitchVerification -eq "No") -or ($SwitchVerification -eq "N"))
+
+#================================================
+
+
+#==================================
+# Read in and validate the CSV file
+#==================================
+do {
+$CsvFailed = $false
+$CsvLocation = Read-Host -Prompt 'Please enter the full path of the CSV file (Eg. c:\Temp\ports.csv)'
+Try{
+    $ports = Import-Csv -Path "$CsvLocation"
+} catch {
+Write-Host "Unable to find or interpret .csv file`n"
+$CsvFailed = $true}
+}
+while ($CsvFailed)
+
+#====================================
+
+
 Write-Host "--------------------------------------------------------"
-Write-Host "`nConfiguring device $serial using configuration from $CsvLocation `n"
+Write-Host "`nConfiguring device $SwitchName `($serial`) using configuration from $CsvLocation `n"
 
 
 #=================================================================
