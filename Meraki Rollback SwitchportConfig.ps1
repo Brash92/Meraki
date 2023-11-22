cacad4a3f98f3ebbfb2f1432885d0db739033bc9
Brash <82854707+Brash92@users.noreply.github.com>
2023-11-23 09:48:31 +1100

    Add files via upload
    Updated files containing logic to perform some extra verifications and take a JSON config backup prior to committing the new config.
    The "Meraki Rollback SwitchportConfig" script can take the backup JSON file and restore it to the switch.

--- Meraki Rollback SwitchportConfig.ps1
+++ Meraki Rollback SwitchportConfig.ps1
@@ -1,11 +1,11 @@
 ﻿# This script is designed to take a CSV file complete with switch port numbers, types, names and vlans, and configure it using the Meraki API
 # Double check all variables before running and USE AT YOUR OWN RISK!
 
-Write-Host "=============================DISCLAIMER==================================================="
-Write-Host "This script is designed to automate switchport configuration based on a provided CSV file."
+Write-Host "=============================DISCLAIMER============================================================"
+Write-Host "This script is designed to automate switchport configuration based on a provided JSON Backup file."
 Write-Host "This is not an official script and comes with no guarantees, warranties or support."
 Write-Host "Use At Your Own Risk!"
-Write-Host "==========================================================================================`n"
+Write-Host "===================================================================================================`n"
 
 
 # Get date for backup filename
@@ -56,6 +56,7 @@ while($serial -notmatch "^([A-Za-z0-9]{4})-([A-Za-z0-9]{4})-([A-Za-z0-9]{4})$")
     $Serial = Read-Host -Prompt 'Please enter the switch serial number (xxxx-xxxx-xxxx)'
 }
 
+
 # Perform a lookup of the SN# to validate it is in the Organisation, and to get the device name
 Try{
     # Set the headers with the API Key
@@ -80,7 +81,7 @@ catch {
       }
 
 # Prompt the user to confirm that this is indeed the switch that they are wanting to make changes on
-
+#if ($SwitchVerification -notmatch "^(?:Yes\b|No\b)")
 if ($SwitchVerification -notmatch "^(?:Yes\b|No\b|yes\b|no\b|Y\b|N\b|y\b|n\b)")
 {
 $SwitchVerification = Read-Host -Prompt "You are about to edit the switch $SwitchName. Type `"Yes`" to proceed or `"No`" to re-enter the Serial Number"
@@ -96,78 +97,52 @@ while (($SwitchVerification -eq "No") -or ($SwitchVerification -eq "N"))
 #================================================
 
 
-#==================================
-# Read in and validate the CSV file
-#==================================
+#===========================================
+# Read in and validate the JSON Backup file
+#===========================================
 do {
-$CsvFailed = $false
-$CsvLocation = Read-Host -Prompt 'Please enter the full path of the CSV file (Eg. c:\Temp\ports.csv)'
+$JsonFailed = $false
+$JsonLocation = Read-Host -Prompt 'Please enter the full path of the JSON file (Eg. c:\Temp\ports.json)'
 Try{
-    $ports = Import-Csv -Path "$CsvLocation"
+    $portConfig = Get-Content -Raw -Path "$JsonLocation" | ConvertFrom-Json
 } catch {
-Write-Host "Unable to find or interpret .csv file`n"
-$CsvFailed = $true}
+Write-Host "Unable to find or interpret .json file`n"
+$JsonFailed = $true}
 }
-while ($CsvFailed)
+while ($JsonFailed)
 
 #====================================
 
-
 #===================================================================
 # Create a backup JSON file of the existing configuration
-# This backup can be re-configured onto the switch using the script:
-# "Meraki Rollback SwitchportConfig.ps1"
 #===================================================================
+
 Write-Host "--------------------------------------------------------`n"
 write-host "Creating backup of existing configuration"
 
-    $backupParentPath = Split-Path -Path $CsvLocation
+    $backupParentPath = Split-Path -Path $JsonLocation
     $backupPath = "$backupParentPath`\$Switchname`_backup`_$date.json"
     $request = Invoke-RestMethod -Method GET -Uri "https://api.meraki.com/api/v1/devices/$Serial/switch/ports" -Headers $Headers
     $request | ConvertTo-Json -depth 100 | Out-File "$backupPath"
 
 write-host "Backup written to $backupPath"
 
-#=================================================================
+#=====================================================================
 
 
-Write-Host "`nConfiguring device $SwitchName `($serial`) using configuration from $CsvLocation `n"
+#============================================
+# Configure switch using provided JSON file
+#============================================
+
+Write-Host "`nConfiguring device $SwitchName `($serial`) using configuration from $JsonLocation `n"
 
 
-#=================================================================
-# Iterate through the ports as per the CSV file, build
-# the JSON with the correct parameters and push the configuration
-#=================================================================
-ForEach ($PortID in $ports){
+ForEach ($port in $portConfig){
 
-# Check if port type is access
-If ($PortID.type -eq "access"){ 
-
-$Body= @{
-"type"="access";
-"vlan"=$PortID.vlan;
-"name"=$PortID.name
+    $portID = $port.portId
+    $Body = $port
+    write-host "Configuring port $portID"
+    $request = Invoke-RestMethod -Method PUT -Uri "https://api.meraki.com/api/v1/devices/$serial/switch/ports/$portID" -Headers $Headers -Body ($Body|ConvertTo-Json)
 }
 
-}
-# Check if port type is trunk
-Elseif ($PortID.type -eq "trunk"){
-$Body= @{
-"type"="trunk";
-"vlan"=$PortID.vlan;
-"name"=$PortID.name;
-"allowedVlans"=$portID.allowedvlans
-}
-}
-
-$PortNumber = $PortID.port
-$vlan=$PortID.vlan
-$name=$PortID.name
-
-Write-Output "Configuring Port $PortNumber with Vlan $vlan and name $name"
-
-# Execute the API call to configure the port
-$request = Invoke-RestMethod -Method PUT -Uri "https://api.meraki.com/api/v1/devices/$Serial/switch/ports/$PortNumber" -Headers $Headers -Body ($Body|ConvertTo-Json)
-
-}
-#======================================================================================
+#============================================
